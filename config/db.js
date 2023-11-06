2efaa525becdbfb52b92df6ffd06bdc42a1ba1db
genstubo <137889448+genstubo@users.noreply.github.com>
2023-11-07 01:47:07 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose
-   .connect(process.env.MONGODB_URI, {
+   .connect(process.MONGODB_URI, {
     
    })
    .then(() => {
